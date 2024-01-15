53f3ad90d3c57900c80bca5f39035fbafc7e5b06
Duy Nguyen <s3878076@rmit.edu.vn>
2024-01-15 14:19:39 +0700

    think

--- index.php
+++ index.php
@@ -90,7 +90,7 @@
             <div class="row rmit-product-section">
               <?php
               $link = mysqli_connect(
-                  "127.0.0.1",
+                  "db-service",
                   "db_admin",
                   "rmit_password",
                   "rmit_store_db"
