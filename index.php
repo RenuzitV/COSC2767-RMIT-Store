0b7a383278325264d86071edf8e40f445abb1ee1
Duy Nguyen <s3878076@rmit.edu.vn>
2024-01-17 16:25:01 +0700

    test update change on github

--- index.php
+++ index.php
@@ -141,7 +141,7 @@
         <footer class="footer_area row">
             <div class="container custom-container">
                 <div class="copy_right_area">
-                    <h4 class="copy_right">© Copyright 2022 RMIT Vietnam | Made with ❤️</h4>
+                    <h4 class="copy_right">© Copyright 2022 RMIT Vietnam | Made with ❤️ and a bit of CI/CD!</h4>
                 </div>
             </div>
         </footer>
